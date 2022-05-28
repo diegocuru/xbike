d9cd7c22e4d551369881aeed90f8c5c5396cb88a
Diego Curumaco <diegocuru@outlook.com>
2022-05-27 22:34:06 -0600

    Added Google Maps iOS SDK

--- XBike/Pods/Target Support Files/Pods-XBike-XBikeUITests/Pods-XBike-XBikeUITests-acknowledgements.markdown
+++ XBike/Pods/Target Support Files/Pods-XBike-XBikeUITests/Pods-XBike-XBikeUITests-acknowledgements.markdown
@@ -1,6 +1,10 @@
 # Acknowledgements
 This application makes use of the following third party libraries:
 
+## GoogleMaps
+
+Copyright 2022 Google
+
 ## SwiftLint
 
 The MIT License (MIT)
--- XBike/XBike/SceneDelegate.swift
+++ XBike/XBike/SceneDelegate.swift
@@ -14,7 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // Use this method to optionally configure and attach the UIWindow
+        // `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         guard scene is UIWindowScene else { return }
     }
@@ -46,7 +47,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-        
         // Save changes in the application's managed object context when the application transitions to the background.
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
